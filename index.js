650f712ce3429257346ae89a5f9de32ef09dbee0
vergaraldvm <leonardo11vergara11@gmail.com>
2022-09-06 23:49:33 -0500

    home + about sections (+/-)

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const http = require('http').Server(app);
 const path = require('path');
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'public');
 
 // use the public folder for static files
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'src')));
 
 // set up routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'src', 'index.html'));
 });
 
 // start the http server
